072488249fa235df06034b1d6e39eb33f27909f8
Mainfast <thomassilva23@hotmail.com>
2021-02-22 15:38:00 -0300

    removido plugin do algolia

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,3 @@
-require("dotenv").config()
-
-const queries = require("./src/utils/algolia_queries")
-
 module.exports = {
   siteMetadata: {
     title: "Allto Digital",
@@ -33,17 +29,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-algolia-search`,
-      options: {
-        appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_ADMIN_KEY,
-        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
-        queries,
-        chunkSize: 10000,
-        enablePartialUpdates: true,
-      },
-    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
--- public/component---src-pages-about-js-3e454b604975c1b0461a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"3XHS":function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),r=t.n(a),c=t("Bl7J"),i=t("vrFN");n.default=function(){return r.a.createElement(c.a,null,r.a.createElement(i.a,{title:"Bio"}))}}}]);
-//# sourceMappingURL=component---src-pages-about-js-3e454b604975c1b0461a.js.map
