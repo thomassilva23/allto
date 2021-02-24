b8e02eeb7484e04650130d74d6450331c85574b8
Mainfast <thomassilva23@hotmail.com>
2021-02-24 17:23:36 -0300

    icone

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,24 +7,26 @@ import Footer from "./elements/footer"
 import "../../static/styles/main.scss"
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+  <>
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <main>{children}</main>
-        <Footer siteTitle={data.site.siteMetadata.title} />
-      </>
-    )}
-  />
+      `}
+      render={data => (
+        <>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <main>{children}</main>
+          <Footer siteTitle={data.site.siteMetadata.title} />
+        </>
+      )}
+    />
+  </>
 )
 
 Layout.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,40 +5,68 @@ import ClientSlider from "../components/body/slider"
 import WhatsappApi from "../components/contact/wpp"
 import Scroll from "../components/body/scroll"
 import SEO from "../components/seo"
+import { Helmet } from "react-helmet"
 
 import Typewriter from "typewriter-effect"
 
 const IndexPage = () => {
   return (
-    <Layout>
-      <SEO title="Allto Digital" />
-      <div className={"page-header home"}>
-        <h1>
-          Digitalização e automação <br /> de processos
-          <Typewriter
-            options={{
-              strings: [
-                "industriais",
-                "administrativos",
-                "financeiros",
-                "de RH",
-                "e outras áreas de back-office",
-              ],
-              autoStart: true,
-              loop: true,
-            }}
-          />
-        </h1>
+    <>
+      <Helmet>
+        {/*jquery*/}
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.0/jquery.min.js" />
+        {/*função logo*/}
+        <script type="text/javascript">{`
+        {
+            $(function() { var logo = $(".logo"); $(window).scroll(function() {
+              var scroll = $(window).scrollTop();
+            
+                if (scroll > 1000) {
+                  if(!logo.hasClass("sml-logo")) {
+                    logo.hide();
+                    logo.removeClass("logo").addClass("sml-logo").fadeIn( "slow");
+                  }
+                } else {
+                  if(!logo.hasClass("logo")) {
+                    logo.hide();
+                    logo.removeClass("sml-logo").addClass("logo").fadeIn( "slow");
+                  }
+                }
+            
+              });
+            }); 
+        }`}</script>
+      </Helmet>
+      <Layout>
+        <SEO title="Allto Digital" />
+        <div className={"page-header home"}>
+          <h1>
+            Digitalização e automação <br /> de processos
+            <Typewriter
+              options={{
+                strings: [
+                  "industriais",
+                  "administrativos",
+                  "financeiros",
+                  "de RH",
+                  "e outras áreas de back-office",
+                ],
+                autoStart: true,
+                loop: true,
+              }}
+            />
+          </h1>
 
-        <br />
-      </div>
+          <br />
+        </div>
 
-      <WhatsappApi />
+        <WhatsappApi />
 
-      <ClientSlider />
+        <ClientSlider />
 
-      <Scroll />
-    </Layout>
+        <Scroll />
+      </Layout>
+    </>
   )
 }
 
