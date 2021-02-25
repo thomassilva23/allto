d0e2a15f76c7741b7c9d9a079f43ab3db87ccdbb
Mainfast <thomassilva23@hotmail.com>
2021-02-25 17:34:26 -0300

    teste form

--- src/components/elements/header.js
+++ src/components/elements/header.js
@@ -1,7 +1,6 @@
 import { Link } from "gatsby"
 import React from "react"
 import logo_header from "../../../static/images/logos/allto.semfundo.png"
-import logo_sml from "../../../static/images/logos/allto.icon.semfundo.png"
 import { Helmet } from "react-helmet"
 
 const Header = () => {
@@ -13,39 +12,36 @@ const Header = () => {
         {/*função logo*/}
         <script type="text/javascript">{`
         {
+  
+          var logo = $(".logo"); 
           
-          var logo = $(".logo"); $(window).scroll(function() {
-          var scroll = $(window).scrollTop();
+          $(window).scroll(function() {
+            var scroll = $(window).scrollTop();
       
-          if (scroll > 700) {
-            if(!logo.hasClass("sml-logo")) {
-              logo.removeClass("logo").addClass("logo_change").fadeIn( "slow");
-              $('#logo_img').attr('src', '/static/allto.icon.semfundo-33d28a632ea797eb194a5809218a07a6.png');
-            }
-          } else {
-            if(!logo.hasClass("logo")) {
-              logo.removeClass("sml-logo").addClass("logo").fadeIn( "slow");
-              $('#logo_img').attr('src', '/static/allto.semfundo-f68c1142a0e319a28a191f8e0d6a7412.png');
-            }
-          }      
-        });
+            if (scroll >= 700) {
+              if(logo.hasClass("logo")) {
+                logo.removeClass("logo").addClass("logo_change").fadeIn( "slow");
+                $('#logo_img').attr('src', '/static/allto.icon.semfundo-33d28a632ea797eb194a5809218a07a6.png');
+              }
+            } 
+            else {
+              if(logo.hasClass("logo_change")) {
+                logo.removeClass("logo_change").addClass("logo").fadeIn( "slow");
+                $('#logo_img').attr('src', '/static/allto.semfundo-f68c1142a0e319a28a191f8e0d6a7412.png');
+              }
+            }      
+          });
         }`}</script>
       </Helmet>
       <header>
         <div className={"container"}>
           <div className={"top-menu"}>
             <div className={"logo"}>
-              {/*<div
-                data-sal="slide-up"
-                data-sal-delay="300"
-                data-sal-easing="ease"
-              >*/}
               <Link to="/" title={"Home"}>
                 <img id={"logo_img"} alt={"Logo"} src={logo_header} />
               </Link>
             </div>
           </div>
-          {/*</div>*/}
         </div>
       </header>
     </>
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,65 +1,104 @@
 import React from "react"
 
+import { Helmet } from "react-helmet"
+
 import Icons from "../components/Icons"
 
 class ContactForm extends React.Component {
   render() {
     return (
-      <form
-        name="contato"
-        method="POST"
-        netlify-honeypot="bot-field"
-        data-netlify="true"
-        action="/thanks"
-      >
-        <input type="hidden" name="bot-field" />
-        <input type="hidden" name="form-name" value="contato" />
+      <>
+        <form
+          id="ajax_form"
+          action="/thanks"
+          name="contato"
+          method="POST"
+          netlify-honeypot="bot-field"
+          data-netlify="true"
+        >
+          <input type="hidden" name="bot-field" />
+          <input type="hidden" name="form-name" value="contato" />
 
-        <div className={"container"}>
-          <div className={"input_form"}>
-            <input type="text" name="field1" placeholder="Seu nome" />
-            <input type="email" name="field2" placeholder="Email" />
-            <textarea name="field3" placeholder="Escreva sua mensagem" />
+          <div className={"container"}>
+            <div className={"input_form"}>
+              <input
+                type="text"
+                name="field1"
+                id="name"
+                placeholder="Seu nome"
+              />
+              <input
+                type="email"
+                name="field2"
+                id="email"
+                placeholder="Email"
+              />
+              <textarea
+                name="field3"
+                id="message"
+                placeholder="Escreva sua mensagem"
+              />
+            </div>
+            <div className={"data_form"}>
+              <h2>Contate-nos!</h2>
+              <p>
+                Gostaríamos muito de ouvir o que você tem a dizer.
+                <br />
+                Entraremos em contato com você dentro de 24 horas!
+                <br />
+                Fique ligado na sua caixa de emails.
+              </p>
+              <ul className={"social"}>
+                <li>
+                  <a href="https://fb.me/allto.digital" title={"Facebook"}>
+                    <Icons.Facebook />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.instagram.com/allto.digital/"
+                    title={"Instagram"}
+                  >
+                    <Icons.Instagram />
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="https://www.linkedin.com/company/allto-digital"
+                    title={"Linedkin"}
+                  >
+                    <Icons.Linkedin />
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className={"data_form"}>
-            <h2>Contate-nos!</h2>
-            <p>
-              Gostaríamos muito de ouvir o que você tem a dizer.
-              <br />
-              Entraremos em contato com você dentro de 24 horas!
-              <br />
-              Fique ligado na sua caixa de emails.
-            </p>
-            <ul className={"social"}>
-              <li>
-                <a href="https://fb.me/allto.digital" title={"Facebook"}>
-                  <Icons.Facebook />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.instagram.com/allto.digital/"
-                  title={"Instagram"}
-                >
-                  <Icons.Instagram />
-                </a>
-              </li>
-              <li>
-                <a
-                  href="https://www.linkedin.com/company/allto-digital"
-                  title={"Linedkin"}
-                >
-                  <Icons.Linkedin />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <button id="btn_send" type="submit">
-          <span class="noselect">Enviar</span>
-          <div id="circle" />
-        </button>
-      </form>
+          <button id="btn_send" type="submit">
+            <span class="noselect">Enviar</span>
+            <div id="circle" />
+          </button>
+
+          <div id="resp" class="form_div" />
+        </form>
+        <Helmet>
+          {/*jquery*/}
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" />
+
+          {/*função para não dar reload*/}
+          <script type="text/javascript">{`
+        {
+          $(document).ready(function(){
+            $("#ajaxform").submit(function(event){
+              var postdata = ("#ajaxform").serialize();
+              $.post("/thanks",postdata, function(response){
+                $("#resp").show().html(response);
+              })
+              return false;
+            })
+          });
+        }`}</script>
+        </Helmet>
+      </>
     )
   }
 }
